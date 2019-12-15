0b29bc5f91fda8cfcd4c94a5e8d0684650f90171
Matheus Castello <matheus@castello.eng.br>
2019-12-15 20:25:51 -0300

    ps: Set all content in a one line
    This remove all comments too
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>
    
    diff --git a/send.ps1 b/send.ps1
    index 8acedb0..f81832f 100644
    --- a/send.ps1
    +++ b/send.ps1
    @@ -8,6 +8,7 @@ try {
     # args
     $projectPath = $args[1];
     $comport = $args[0]
    +$pathIndex = -join($PSScriptRoot, "\index.js")
    
     if ($projectPath -eq $null -or $comport -eq $null) {
         throw [System.ArgumentException] 'use: .\send.ps1 COM4 ./blink/'
    @@ -16,10 +17,25 @@ if ($projectPath -eq $null -or $comport -eq $null) {
     # first put all js together
     Get-ChildItem -Path $projectPath -Filter *.js -Recurse -File -Name| ForEach-Object {
         $filename = [string]([System.IO.Path]::GetFileName($_))
    -    $path = -join($projectPath, $filename)
    -    cat $path >> index.js
    +    if ($filename -ne "index.js") {
    +        $path = -join($projectPath, $filename)
    +        cat $path >> index.js
    +    }
     }
    
    +# now the index
    +Get-ChildItem -Path $projectPath -Filter index.js -Recurse -File -Name| ForEach-Object {
    +    $filename = [string]([System.IO.Path]::GetFileName($_))
    +    if ($filename -eq "index.js") {
    +        $path = -join($projectPath, $filename)
    +        cat $path >> index.js
    +    }
    +}
    +
    +# remove comments
    +Set-Content -Path $pathIndex -Value (get-content -Path $pathIndex | Select-String -Pattern '//' -NotMatch)
    +Set-Content -Path $pathIndex -Value (get-content -Path $pathIndex | Select-String -Pattern '/\*' -NotMatch)
    +
     # Open and configure the serial port
     Write-Host '🤔 Try to connect to '$comport
    
    @@ -31,13 +47,36 @@ Write-Host '😀 Connected to '$comport
     # write the file content to the espruino
     $port.WriteLine('reset();')
     Start-Sleep -Milliseconds 500
    +
     $pathfile = $PSScriptRoot + '\index.js'
     $content = [IO.File]::ReadAllText($pathfile)
    -$port.WriteLine($content)
    +$charWrite = 0
    +$charCount = $content.Length
    +
    +Write-Host -NoNewLine '⏲ Writing .'
    +
    +for ($i=0; $i -lt $charCount; $i++) {
    +    if (($content[$i] -ne "`n") -and ($content[$i] -ne "`t") -and ($content[$i] -ne "`r")) {
    +        $port.Write($content[$i])
    +        #Write-Host -NoNewLine $content[$i]
    +    } else {
    +        $port.Write(' ')
    +        #Write-Host -NoNewLine ' '
    +        Start-Sleep -Milliseconds 10
    +    }
    +    Write-Host -NoNewLine '.'
    +}
    +Write-Host ''
    +Write-Host '👌 Done'
    
     # ask espruino to save
    +$port.WriteLine(' ')
     $port.WriteLine('save();')
    
    +# sanity
    +Start-Sleep -Milliseconds 1000
    +$port.WriteLine('load();')
    +
     # all done close connection
     $port.Close()
    -Write-Host '😎 Closed '$comport
    \ No newline at end of file
    +Write-Host '😎 Closed '$comport

--- send.ps1
+++ send.ps1
@@ -8,6 +8,7 @@ try {
 # args
 $projectPath = $args[1];
 $comport = $args[0]
+$pathIndex = -join($PSScriptRoot, "\index.js")
 
 if ($projectPath -eq $null -or $comport -eq $null) {
     throw [System.ArgumentException] 'use: .\send.ps1 COM4 ./blink/'
@@ -16,9 +17,24 @@ if ($projectPath -eq $null -or $comport -eq $null) {
 # first put all js together
 Get-ChildItem -Path $projectPath -Filter *.js -Recurse -File -Name| ForEach-Object {
     $filename = [string]([System.IO.Path]::GetFileName($_))
-    $path = -join($projectPath, $filename)
-    cat $path >> index.js
+    if ($filename -ne "index.js") {
+        $path = -join($projectPath, $filename)
+        cat $path >> index.js
+    }
 }
+
+# now the index
+Get-ChildItem -Path $projectPath -Filter index.js -Recurse -File -Name| ForEach-Object {
+    $filename = [string]([System.IO.Path]::GetFileName($_))
+    if ($filename -eq "index.js") {
+        $path = -join($projectPath, $filename)
+        cat $path >> index.js
+    }
+}
+
+# remove comments
+Set-Content -Path $pathIndex -Value (get-content -Path $pathIndex | Select-String -Pattern '//' -NotMatch)
+Set-Content -Path $pathIndex -Value (get-content -Path $pathIndex | Select-String -Pattern '/\*' -NotMatch)
 
 # Open and configure the serial port
 Write-Host '🤔 Try to connect to '$comport
@@ -31,12 +47,35 @@ Write-Host '😀 Connected to '$comport
 # write the file content to the espruino
 $port.WriteLine('reset();')
 Start-Sleep -Milliseconds 500
+
 $pathfile = $PSScriptRoot + '\index.js'
 $content = [IO.File]::ReadAllText($pathfile)
-$port.WriteLine($content)
+$charWrite = 0
+$charCount = $content.Length
+
+Write-Host -NoNewLine '⏲ Writing .'
+
+for ($i=0; $i -lt $charCount; $i++) {
+    if (($content[$i] -ne "`n") -and ($content[$i] -ne "`t") -and ($content[$i] -ne "`r")) {
+        $port.Write($content[$i])
+        #Write-Host -NoNewLine $content[$i]
+    } else {
+        $port.Write(' ')
+        #Write-Host -NoNewLine ' '
+        Start-Sleep -Milliseconds 10
+    }
+    Write-Host -NoNewLine '.'
+}
+Write-Host ''
+Write-Host '👌 Done'
 
 # ask espruino to save
+$port.WriteLine(' ')
 $port.WriteLine('save();')
+
+# sanity
+Start-Sleep -Milliseconds 1000
+$port.WriteLine('load();')
 
 # all done close connection
 $port.Close()
